9f10e14107e0c0ff4d0eb723ba213e06f1497ecb
nao3301 <nao.lupa@gmail.com>
2018-02-24 19:46:40 +0100

    Update wApp.js

--- wApp.js
+++ wApp.js
@@ -2,6 +2,11 @@ const restify = require('restify');
 require('dotenv').config();
 const bot = require('./bot.js');
 
+const connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
 const server = restify.createServer();
 server.post('/api/messages', bot.connector('*').listen());
 server.listen(process.env.PORT, () => {
